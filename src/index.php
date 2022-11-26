926c666c07883190fae10ad2b7b4421d3a2f5197
josevictormoreno <josevictor.admoreno@gmail.com>
2022-11-26 12:45:58 -0300

    adds login and index php pages

--- src/index.php
+++ src/index.php
@@ -7,13 +7,34 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Programação web</title>
+  <link href="style.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 
 <!-- style -->
 
 <body>
-  
-<!-- scripts -->
+<nav style="background-color:#912437" class="navbar navbar-expand-lg navbar-dark">
+  <a class="navbar-brand" style="margin-left:1rem" href="#">Emprestimos</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <span class="spacer"></span>
+    <ul style="margin-right:1.5rem" class="navbar-nav">
+      <li class="nav-item active">
+      <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#">Conta</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 </body>
 </html>
